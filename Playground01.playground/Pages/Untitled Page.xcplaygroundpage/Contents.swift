ced179d5c77d31090ed78304045920e523586c3d
luisagalva <luisabeyu200797@gmail.com>
2018-01-11 16:42:47 -0600

    Actividad 1

---
+++ Playground01.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var v1 = "esto es swift" //asignación del texto esto es Swift a la variable v1
+
+let c1 = 4 //asignación del número 4 a la constante c1
+
+var cadena:String = String() //asignación formal de un tipo de dato String
+
+cadena = "Hola mundo " + v1 //asignación (concatenada) de dos cadenas
+
+print("El contenido es: \(cadena)") //salida a consola y la ventana de resultado con el valor contenido entre los paréntesis
+
+print("El valor de constante es: \(c1)")
