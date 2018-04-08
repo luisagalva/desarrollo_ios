e38f9a45f420afa309fddf555194758ea9915973
luisagalva <luisabeyu200797@gmail.com>
2018-04-08 00:08:03 -0500

    Act11

---
+++ Prototipo/Prototipo/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Prototipo
+//
+//  Created by Luisa Beatriz Garcia Alva on 07/04/18.
+//  Copyright © 2018 Luisa Beatriz Garcia Alva. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagenNum: UIImageView!
+    
+}
